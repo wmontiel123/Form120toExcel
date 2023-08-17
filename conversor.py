1275eb149fecb67e396d573cd6cdccd1ada8d505
wmontiel123 <lorenzohyuga@gmail.com>
2023-08-17 17:17:08 -0400

    regimen exportador

--- conversor.py
+++ conversor.py
@@ -6,6 +6,14 @@ from tkinter import filedialog
 from tkinter.filedialog import askdirectory
 import os
 
+
+def convertir_fecha(fecha):
+    partes = fecha.split()
+    mes = partes[0].rjust(2, '0')
+    anio = ''.join(partes[2:])
+    fecha_convertida = f"{mes}-{anio}"
+    return fecha_convertida
+
 def get_pdf_files(folder_path):
     pdf_files = []
     for file_name in os.listdir(folder_path):
@@ -20,6 +28,8 @@ def leer_pdf(ruta):
         for pagina in lector_pdf.pages:
             contenido = pagina.extract_text()
             lineas.extend(contenido.split('\n'))
+            
+
     return lineas
 
 def extract_text(linea, inicio, fin1, fin2):
@@ -65,15 +75,11 @@ columna2 = 2
 # Crear un nuevo libro de Excel y seleccionar la hoja activa
 wb = Workbook()
 sheet = wb.active
-
 for file_path in pdf_files:
     # Print the selected file path
    
-    ruta_pdf = folder_path+'\\'+ file_path
-    
-    #Carga de Valores Para La Carga
+    ruta_pdf = folder_path+'\\'+ file_path  
     Valores_Obtenidos = []
-    
     values = [
         ("vados con tasa del 10%10", " 22 ", " 022 "),
         (" 22 ", "", ""),
@@ -161,8 +167,11 @@ for file_path in pdf_files:
         ("Impuesto63", "", ""),
         ("IRP 64 ", "", ""),
         ("IRP170 ", "", ""),
-        
-        
+        ("PERIODO ATRIBUIDAS A EXPORTACION", "", ""),
+        ("industrializacion171", " 181 ", "0181 "),
+        ("exportacion172", " 177 ", "0177 "),
+        ("anticipadamente173", " 183 ", "0183 ")
+        ("anticipadamente174", " 183 ", "0183 "),
     ]
 
 
@@ -177,14 +186,26 @@ for file_path in pdf_files:
     Orden = ''
     Mes_Año = ''
     Tipo_declaracion = ''
-    
+    Normalizada = ''
     
     for i in range(len(lineas_pdf)-1):
         if lineas_pdf[i].startswith("Mes Año"):
             Mes_Año = lineas_pdf[i+1]
             break
     
+    
     for line in lineas_pdf:
+        if "DECLARACIÓN JURADA NORMALIZADA" in line:
+            if "DECLARACIÓN JURADA NORMALIZADA" in line:
+                Normalizada = 'DECLARACIÓN JURADA NORMALIZADA'
+            else:
+                Normalizada = 'DECLARACIÓN JURADA ORIGINAL'
+                
+            break
+        
+    
+    for line in lineas_pdf:
+        print(line)
         
         if line.startswith("Número de Orden"):
             Orden = re.search(r'Número de Orden\s+(\d+)', line)
@@ -301,28 +322,75 @@ for file_path in pdf_files:
             "62",
             "63",
             "64",
-            "170"]
+            "170",
+            "171",
+            "172",
+            "173",
+            "174",
+            "175",
+            "176",
+            "177",
+            "178",
+            "179",
+            "180",
+            "181",
+            "182",
+            "183",
+            "184",
+            "185",
+            "186",
+            "187",
+            "188",
+            "189",
+            "190",
+            "191",
+            "192",
+            "193",
+            "194",
+            "195",
+            "196",
+            "197",
+            "198",
+            "199",
+            "200",
+            "201",
+            "202",
+            "203",
+            "204",
+            "205",
+            "206",
+            "207",
+            "208",
+            "209",
+            "210",
+            "211",
+            "212",
+            "213",
+            "214",
+            "215"
+            ]
 
     # Copiar los datos en el excel
     
-    sheet.cell(row = 1, column = columna1).value = 'Contribuyente'
-    sheet.cell(row = 2, column = columna1).value = 'Control'
-    sheet.cell(row = 3, column = columna1).value = 'Fecha/Hora'
-    sheet.cell(row = 4, column = columna1).value = 'Numero de Orden'
-    sheet.cell(row = 5, column = columna1).value = 'Mes/Año'
-    sheet.cell(row = 6, column = columna1).value = 'Tipo Declaracion'
+    sheet.cell(row = 1, column = columna1).value =  Normalizada
+    sheet.cell(row = 2, column = columna1).value = 'Contribuyente'
+    sheet.cell(row = 3, column = columna1).value = 'Control'
+    sheet.cell(row = 4, column = columna1).value = 'Fecha/Hora'
+    sheet.cell(row = 5, column = columna1).value = 'Numero de Orden'
+    sheet.cell(row = 6, column = columna1).value = 'Mes/Año'
+    sheet.cell(row = 7, column = columna1).value = 'Tipo Declaracion'
     
-    
-    sheet.cell(row = 1, column = columna2).value = contribuyente
-    sheet.cell(row = 2, column = columna2).value = control
-    sheet.cell(row = 3, column = columna2).value = fecha
-    sheet.cell(row = 4, column = columna2).value = Orden
-    sheet.cell(row = 5, column = columna2).value = Mes_Año
+    sheet.cell(row = 1, column = columna2).value = ''
+    sheet.cell(row = 2, column = columna2).value = contribuyente
+    sheet.cell(row = 3, column = columna2).value = control
+    sheet.cell(row = 4, column = columna2).value = convertir_fecha(fecha)
+    sheet.cell(row = 5, column = columna2).value = Orden
+    sheet.cell(row = 6, column = columna2).value = Mes_Año
     
     if Tipo_declaracion == '0':
-        sheet.cell(row = 6, column = columna2).value = 'Original'
+        sheet.cell(row = 6, column = columna2).value = 'ORIGINAL'
     else:
-        sheet.cell(row = 6, column = columna2).value = 'Rectificativa'
+        sheet.cell(row = 6, column = columna2).value = 'RECTIFICATIVA'
     
     for i, dato in enumerate(datos, start=7):
         sheet.cell(row=i, column = columna1).value = dato
